3666f2244205bca03e89cbd25ecab0bbd0ff572a
Niels Abildgaard <niels.abildgaard@gmail.com>
2016-07-23 21:01:27 +0200

    Extract renderView to file

---
+++ renderView.js
@@ -0,0 +1,19 @@
+var frontmatter = require("frontmatter");
+var mustache = require("mustache");
+
+function renderView(view, callback) {
+    var page = frontmatter(view);
+    if(!page.data || !page.data.layout) {
+        return callback(null, view);
+    }
+    var layoutPath = path.join(__dirname, "layouts", page.data.layout + ".html");
+    fs.readFile(layoutPath, function(error, layoutBuf) {
+        if(error) {
+            return callback(error);
+        }
+        var merged = mustache.render(layoutBuf.toString(), { content: page.content });
+        renderView(merged, callback);
+    });
+}
+
+module.exports = renderView;
--- static-view-endpoint.js
+++ static-view-endpoint.js
@@ -1,7 +1,6 @@
 var fs = require("fs");
 var path = require("path");
-var frontmatter = require("frontmatter");
-var mustache = require("mustache");
+var renderView = require("./renderView.js");
 
 function staticViewEndpoint(view) {
     return function(req, res) {
@@ -22,21 +21,6 @@ function respondWithView(view, res) {
             }
             res.send(result);
         });
-    });
-}
-
-function renderView(view, callback) {
-    var page = frontmatter(view);
-    if(!page.data || !page.data.layout) {
-        return callback(null, view);
-    }
-    var layoutPath = path.join(__dirname, "layouts", page.data.layout + ".html");
-    fs.readFile(layoutPath, function(error, layoutBuf) {
-        if(error) {
-            return callback(error);
-        }
-        var merged = mustache.render(layoutBuf.toString(), { content: page.content });
-        renderView(merged, callback);
     });
 }
 
