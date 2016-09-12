461aac9706a5abe595ad42e24f6b59e8930ec36a
Niels Abildgaard <niels.abildgaard@gmail.com>
2016-09-12 19:09:51 +0200

    Use postgres for listing subscribers

--- prelaunch/list-subscribers-endpoint.js
+++ prelaunch/list-subscribers-endpoint.js
@@ -3,24 +3,24 @@ var async = require("async");
 var fs = require("fs");
 var path = require("path");
 var renderView = require("../renderView.js");
+var Pool = require("pg-pool");
+var config = require("config");
 
 module.exports = function(req, res) {
     if(req.query.secret != "1241215124125123122215124") {
         return res.fail(400, "You don't know the secret, bruv.");
     }
-    kvfs.list("subscription", function(error, subscriptions) {
+    var pool = new Pool(config.postgres);
+    pool.query("SELECT * FROM user_signed_up_for_newsletter", (error, result) => {
         if(error) {
             console.error("Failed to get subscription list", error);
             return res.fail(500);
         }
-        async.map(subscriptions, function(subscription, callback) {
-            kvfs.get(subscription, callback);
-        }, function(error, subscriptionContents) {
-            subscriptionContents = subscriptionContents.filter(x => x.state == "subscribed");
-            if(error) {
-                console.error("Failed to read one or more subscriptions", error);
-                return res.fail(500);
-            }
+        var subscriptionContents = result.rows.map(row => {
+            return {
+                email: row.data.email
+            };
+        });
             fs.readFile(path.join(__dirname, "list-subscribers-view.html"), function(error, viewBuf) {
                 if(error) {
                     console.error("Failed to read list-subscribers-view", error);
@@ -34,6 +34,5 @@ module.exports = function(req, res) {
                     res.send(response);
                 });
             });
-        });
     });
 };
