641d598f638d983c6bd6842dd0aabd7bb15dcb41
Niels Abildgaard <niels.abildgaard@gmail.com>
2016-09-17 16:51:10 +0200

    Extract getActiveSubscription

--- prelaunch/list-subscribers-endpoint.js
+++ prelaunch/list-subscribers-endpoint.js
@@ -8,10 +8,31 @@ function listSubscribersEndpoint(pool, req, res) {
     if(req.query.secret != "1241215124125123122215124") {
         return res.fail(400, "You don't know the secret, bruv.");
     }
+    getActiveSubscriptions(pool, (error, subscriptions) => {
+        if(error) {
+            return res.fail(500);
+        }
+            fs.readFile(path.join(__dirname, "list-subscribers-view.html"), function(error, viewBuf) {
+                if(error) {
+                    console.error("Failed to read list-subscribers-view", error);
+                    return res.fail(500);
+                }
+                renderView(viewBuf.toString(), { subscribers: subscriptions }, function(error, response) {
+                    if(error) {
+                        console.error("Failed to render subscriber list", error);
+                        return res.fail(500);
+                    }
+                    res.send(response);
+                });
+            });
+    });
+}
+
+function getActiveSubscriptions(pool, callback) {
     pool.query("SELECT * FROM user_signed_up_for_newsletter", (error, result) => {
         if(error) {
-            console.error("Failed to get subscription list", error);
-            return res.fail(500);
+            console.error("[getActiveSubscriptions] Failed to get subscription list", error);
+            return callback(error);
         }
         var subscriptionContents = result.rows.map(row => {
             return {
@@ -21,8 +42,8 @@ function listSubscribersEndpoint(pool, req, res) {
         });
         pool.query("SELECT * FROM user_unsubscribed_from_newsletter", (error, result) => {
             if(error) {
-                console.error("Failed to check who has unsubscribed for subscriber list", error);
-                return res.fail(500);
+                console.error("[getActiveSubscriptions] Failed to check who has unsubscribed for subscriber list", error);
+                return callback(error);
             }
             var unsubscribedEmails = result.rows.map(row => {
                 var match = subscriptionContents.filter(subscription => subscription.id == row.id)[0];
@@ -39,19 +60,7 @@ function listSubscribersEndpoint(pool, req, res) {
                 alreadyIncludedEmails.push(subscription.email);
                 return true;
             });
-            fs.readFile(path.join(__dirname, "list-subscribers-view.html"), function(error, viewBuf) {
-                if(error) {
-                    console.error("Failed to read list-subscribers-view", error);
-                    return res.fail(500);
-                }
-                renderView(viewBuf.toString(), { subscribers: subscriptionContents }, function(error, response) {
-                    if(error) {
-                        console.error("Failed to render subscriber list", error);
-                        return res.fail(500);
-                    }
-                    res.send(response);
-                });
-            });
+            callback(null, subscriptionContents);
         });
     });
 }
