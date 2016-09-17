4ec54b83a99f51c5ee93e61aa674c0ed085e7ff5
Niels Abildgaard <niels.abildgaard@gmail.com>
2016-09-17 17:50:54 +0200

    When building list-subscribers test if an up-to-date projection exists, use it; otherwise build new projection, save and use it.
    Should save some processing time when accessing list-subscribers often with no new changes inbetween.
    This is a test of how to generally build projections on-the-fly-as-needed, which is pretty resource heavy but good enough for now.

--- prelaunch/list-subscribers/endpoint.js
+++ prelaunch/list-subscribers/endpoint.js
@@ -5,6 +5,8 @@ var path = require("path");
 var renderView = require("../../renderView.js");
 var buildSubscriptionsState = require("./buildSubscriptionsState.js");
 var ensureSubscriptionsProjection = require("./ensureSubscriptionsProjection.js");
+var getCurrentStateAndLastEventTimestamp = fs.readFileSync(path.join(__dirname, "getCurrentStateAndLastEventTimestamp.sql")).toString();
+var updateSubscriptionsProjectionIfExists = fs.readFileSync(path.join(__dirname, "updateSubscriptionsProjectionIfExists.sql")).toString();
 
 function listSubscribersEndpoint(pool, req, res) {
     if(req.query.secret != "1241215124125123122215124") {
@@ -12,6 +14,7 @@ function listSubscribersEndpoint(pool, req, res) {
     }
     getActiveSubscriptions(pool, (error, subscriptions) => {
         if(error) {
+            console.error("Failed to get active subscriptions", error);
             return res.fail(500);
         }
         fs.readFile(path.join(__dirname, "view.html"), function(error, viewBuf) {
@@ -31,7 +34,29 @@ function listSubscribersEndpoint(pool, req, res) {
 }
 
 function getActiveSubscriptions(pool, callback) {
-    buildSubscriptionsState(pool, callback);
+    pool.query(getCurrentStateAndLastEventTimestamp, (error, result) => {
+        if(error) {
+            return callback(error);
+        }
+        result = result.rows[0];
+        if(!result) {
+            return callback(new Error("Empty result returned when trying to get last state and event timestamp"));
+        }
+        if(result.last_subscribe_happened_at <= result.last_projection_update_at && result.last_unsubscribe_happened_at <= result.last_projection_update_at) {
+            return callback(null, result.state);
+        }
+        buildSubscriptionsState(pool, (error, state) => {
+            if(error) {
+                return callback(error);
+            }
+            pool.query(updateSubscriptionsProjectionIfExists, [JSON.stringify(state), new Date().toISOString()], (error) => {
+                if(error) {
+                    return callback(error);
+                }
+                callback(null, state);
+            });
+        });
+    });
 }
 
 module.exports = function(pool) {
