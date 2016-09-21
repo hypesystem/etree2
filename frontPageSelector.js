1bceb5f28ef522f9ed128aa6cc653686486e2b4c
Niels Abildgaard <niels.abildgaard@gmail.com>
2016-09-21 09:19:12 +0200

    Add a sales endpoint, accessible from /?forceState=salesStarted

--- frontPageSelector.js
+++ frontPageSelector.js
@@ -1,6 +1,12 @@
 var staticViewEndpoint = require("./staticViewEndpoint.js");
 
 function frontPageSelector(req, res) {
+    //TODO: On certain dates to certain things.
+    if(req.query.forceState == "salesStarted") {
+        var sales = staticViewEndpoint("sales/view.html");
+        return sales(req, res);
+    }
+    
     var prelaunch = staticViewEndpoint("prelaunch/view.html");
     prelaunch(req, res);
 }
