91c90e6ca1c987f4ff61d6c843135df9f7f8d11b
Niels Abildgaard <niels.abildgaard@gmail.com>
2016-10-04 21:22:38 +0200

    fix indentation

--- admin/endpoint.js
+++ admin/endpoint.js
@@ -4,19 +4,19 @@ var fs = require("fs");
 var renderView = require("../renderView.js");
 
 function adminEndpoint(pool, req, res) {
-        fs.readFile(path.join(__dirname, "adminView.html"), (error, buf) => {
+    fs.readFile(path.join(__dirname, "adminView.html"), (error, buf) => {
+        if(error) {
+            console.error("Failed to read adminView", error);
+            return res.fail(500);
+        }
+        renderView(buf.toString(), { admin: req.currentAdmin }, (error, result) => {
             if(error) {
-                console.error("Failed to read adminView", error);
+                console.error("failed to render adminView", error);
                 return res.fail(500);
             }
-            renderView(buf.toString(), { admin: req.currentAdmin }, (error, result) => {
-                if(error) {
-                    console.error("failed to render adminView", error);
-                    return res.fail(500);
-                }
-                res.send(result);
-            });
+            res.send(result);
         });
+    });
 }
 
 module.exports = function(pool) {
