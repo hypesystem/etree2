c5957b67c12848cf4ed46b2cc5171bd77857f0cc
Niels Abildgaard <niels.abildgaard@gmail.com>
2016-10-05 09:56:20 +0200

    Add logout endpoint

--- admin/app.js
+++ admin/app.js
@@ -4,6 +4,7 @@ var adminAuthEndpoint = require("./authEndpoint.js");
 var authenticate = require("./authenticate.js");
 var ensureFirstAdmin = require("./ensureFirstAdmin.js");
 var listSubscribersEndpoint = require("./list-subscribers/endpoint.js");
+var logoutEndpoint = require("./logoutEndpoint.js");
 
 function createAdminApp(pool) {
     var app = express();
@@ -11,6 +12,7 @@ function createAdminApp(pool) {
     app.get("/", authenticate(pool), adminEndpoint(pool));
     app.post("/", adminAuthEndpoint(pool));
     app.get("/list-subscribers", authenticate(pool), listSubscribersEndpoint(pool));
+    app.get("/logout", authenticate(pool), logoutEndpoint);
     
     return app;
 }
