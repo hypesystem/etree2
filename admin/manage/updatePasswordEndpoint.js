3af1f4f4e055703536c464f112ea8b530d1b777b
Niels Abildgaard <niels.abildgaard@gmail.com>
2016-10-11 17:03:57 +0200

    Implement updatePasswordEndpoint

--- admin/manage/updatePasswordEndpoint.js
+++ admin/manage/updatePasswordEndpoint.js
@@ -1,5 +1,26 @@
+var fs = require("fs");
+var path = require("path");
+var updateAdminSql = fs.readFileSync(path.join(__dirname, "updateAdmin.sql"));
+var uuid = require("uuid");
+var crypto = require("crypto");
+
 function updatePasswordEndpoint(pool, req, res) {
-    return res.redirect("/admin");
+    if(!req.body.password || req.body.password == "") {
+        return res.fail(400, "Du mangler at udfylde et password, du vil skifte til.");
+    }
+    var salt = uuid.v4();
+    var hashedInputtedPassword = crypto.createHash("sha256").update(salt + req.body.password).digest("hex");
+    pool.query(updateAdminSql, [
+        req.currentAdmin.id,
+        JSON.stringify({ salt: salt, password: hashedInputtedPassword }),
+        new Date().toISOString()
+    ], error => {
+        if(error) {
+            console.error("Failed to update password for " + currentAdmin.username + "/" + currentAdmin.id + " (insert event failed)", error);
+            return res.fail(500);
+        }
+        res.redirect("/admin/manage");
+    });
 }
 
 module.exports = function(pool) {
