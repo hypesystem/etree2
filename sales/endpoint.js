7171c80f216c996ff2dddb84ec4f20485e940005
Niels Abildgaard <niels.abildgaard@gmail.com>
2016-10-22 20:47:42 +0200

    a bunch of work on the braintree implementation, this part is mostly done!
    it posts around the right way
    all the sql stuff is still absent tho

--- sales/endpoint.js
+++ sales/endpoint.js
@@ -1,17 +1,110 @@
 var uuid = require("uuid");
+var renderView = require("../renderView.js");
+var fs = require("fs");
+var path = require("path");
 
 function salesEndpoint(pool, paymentGateway, req, res) {
+    if(req.body.payment_method_nonce) {
+        completePayment(pool, paymentGateway, req, res);
+    }
+    startPayment(pool, paymentGateway, req, res);
+}
+
+function startPayment(pool, paymentGateway, req, res) {
+    //TODO: validate form input
     //generate client token
-    var customerId = uuid.v4();
-    paymentGateway.generate({
-        customerId: customerId
-    }, (error, response) => {
+    paymentGateway.clientToken.generate({}, (error, response) => {
         if(error) {
             console.error("Failed to generate client token for payment", error);
             return res.fail(500);
         }
-        var clientToken = response.clientToken;
-        res.send("hello " + clientToken);
+        if(!response.success) {
+            console.error("Failed to generate client token from braintree", response);
+            return res.fail(500);
+        }
+        var viewModel = {
+            clientToken: response.clientToken,
+            transactionId: uuid.v4()
+        };
+        //TODO: Insert payment started event with amount etc.
+        fs.readFile(path.join(__dirname, "paymentForm.html"), (error, buf) => {
+            if(error) {
+                console.error("Failed to read paymentForm view", error);
+                return res.fail(500);
+            }
+            renderView(buf.toString(), viewModel, (error, result) => {
+                if(error) {
+                    console.error("Failed to build paymentForm view", error);
+                    return res.fail(500);
+                }
+                res.send(result);
+            });
+        });
+    });
+}
+
+function completePayment(pool, paymentGateway, req, res) {
+    console.log("got", req.body);
+    var nonce = req.body.payment_method_nonce;
+    if(!nonce) {
+        return res.fail(400, "No nonce given!");
+    }
+    var transactionId = req.body.transaction_id;
+    if(!transactionId) {
+        return res.fail(400, "No transactionId given");
+    }
+    pool.query("SELECT * FROM payment_started WHERE id = $1::uuid", [ transactionId ], (error, result) => {
+        if(error) {
+            console.error("Failed to get started payment for id", transactionId, error);
+            return res.fail(500);
+        }
+        if(result.rows.length == 0) {
+            console.log("Got request for non-exist transaction id", transactionId);
+            return res.fail(400, "Forkert transaktions-ID (findes ikke)");
+        }
+        var amount = result.rows[0].data.amount;
+        gateway.transaction.sale({
+            amount: amount,
+            paymentMethodNonce: nonce,
+            options: {
+                submitForSettlement: true
+            }
+        }, (error, result) => {
+            if(error) {
+                console.error("Failed to create sale", error);
+                return res.fail(500);
+            }
+            var transactionData = {
+                amount: amount,
+                nonce: nonce,
+                transaction: result.transaction
+            };
+            if(!result.success) {
+                console.error("Failed to create sale (response success false)", result.errors.deepErrors());
+                return pool.query("INSERT INTO payment_failed (id, data, happened_at) VALUES ($1::uuid, $2::json, $3::timestamp)", [
+                    transactionId,
+                    JSON.stringify(transactionData),
+                    new Date().toISOString()
+                ], (error) => {
+                    if(error) {
+                        console.error("Failed to insert payment failed into db", error);
+                    }
+                    res.fail(500);
+                });
+            }
+            pool.query("INSERT INTO payment_completed (id, data, happened_at) VALUES ($1::uuid, $2::json, $3::timestamp)", [
+                transactionId,
+                JSON.stringify(transactionData),
+                new Date().toISOString()
+            ], (error) => {
+                if(error) {
+                    console.error("Failed to save succesful transaction " + transactionId + " in db", error, transactionData);
+                    return res.fail(500);
+                }
+                //TODO: Send faktura email
+                res.redirect("/tak-for-handlen");
+            });
+        });
     });
 }
 
