414a64beb553381784a8d923dfaaad06515b393b
Niels Abildgaard <niels.abildgaard@gmail.com>
2016-10-25 17:41:56 +0200

    create voucher every time a user signs up for newsletter

--- prelaunch/subscribe-endpoint.js
+++ prelaunch/subscribe-endpoint.js
@@ -4,8 +4,9 @@ var fs = require("fs");
 var path = require("path");
 var addUserSubscribedEventSql = fs.readFileSync(path.join(__dirname, "addUserSubscribed.sql")).toString();
 var ensureSubscriberVouchers2016 = require("./ensureSubscriberVouchers2016/ensure.js");
+var Vouchers = require("../voucher/voucher.js");
 
-function subscribeEndpoint(pool, req, res) {
+function subscribeEndpoint(pool, vouchers, req, res) {
     if(!req.body.email) {
         return res.fail(400, "Ingen email blev angivet.");
     }
@@ -20,6 +21,18 @@ function subscribeEndpoint(pool, req, res) {
         }
         //TODO: Send email to admin with notif
         res.redirect("/du-er-paa-listen");
+        
+        //Create voucher (after redir)
+        var year = new Date().getFullYear();
+        vouchers.create(year + "-" + id, {
+            name: "15% rabat på levering",
+            rebate: 15,
+            filters: [ "delivery" ]
+        }, (error) => {
+            if(error) {
+                console.error("Failed to create prelaunch voucher for " + subscriber.id, error);
+            }
+        });
     });
 }
 
@@ -30,5 +43,6 @@ function isValidEmail(email) {
 module.exports = function(pool) {
     createUserSubscriptionTableIfNotExists(pool);
     ensureSubscriberVouchers2016(pool);
-    return subscribeEndpoint.bind(this, pool);
+    var vouchers = new Vouchers(pool);
+    return subscribeEndpoint.bind(this, pool, vouchers);
 };
