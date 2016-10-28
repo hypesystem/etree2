0779ace4186c53b939ee337ac6909ec693bafdfe
Niels Abildgaard <niels.abildgaard@gmail.com>
2016-10-28 10:11:45 +0200

    Implement automatic frontpage selector depending on date

--- frontPageSelector.js
+++ frontPageSelector.js
@@ -4,15 +4,37 @@ var downtime = staticViewEndpoint("downtime/view.html");
 var prelaunch = staticViewEndpoint("prelaunch/view.html");
 
 function frontPageSelector(req, res) {
-    //TODO: On certain dates to certain things.
     if(req.query.forceState == "salesStarted") {
         return sales(req, res);
     }
     if(req.query.forceState == "salesEnded") {
         return downtime(req, res);
     }
+    if(req.query.forceState == "prelaunch") {
+        return prelaunch(req, res);
+    }
     
-    prelaunch(req, res);
+    var rn = new Date();
+    var humanMonth = rn.getMonth() + 1;
+    var humanDate = rn.getDate();
+    
+    //If we're after sale ends
+    if(humanMonth >= 12 && humanDate >= 23) {
+        return downtime(req, res);
+    }
+    
+    //If we're after sale starts
+    if(humanMonth >= 11) {
+        return sales(req, res);
+    }
+    
+    //If we're in or later than september
+    if(humanMonth >= 9) {
+        return prelaunch(req, res);
+    }
+    
+    //Otherwise  (before september)  we're in downtime
+    return downtime(req, res);
 }
 
 module.exports = frontPageSelector;
